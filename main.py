b56106e1bd93b386c4899cd1d10f31e26a79514f
NadulaG <nadula.gardiyehewa@gmail.com>
2024-09-28 15:26:09 -0400

    'Template-ified' dashboard

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def dashboard():
-    return render_template('dashboard.html')
+    speed = 50
+    temperature = 25
+    soc = 80
+    odometer = 5
+    acceleration = 2
+    throttle = 77
+    brake = 19
+    time_current = datetime.now().strftime("%H:%M")
+    return render_dashboard(speed, temperature, soc, odometer, acceleration, throttle, brake, time_current)
+
+def render_dashboard(speed, temperature, soc, odometer, acceleration, throttle, brake, time_current):
+    return render_template('dashboard.html', speed=speed, temperature=temperature, soc=soc, odometer=odometer,
+                           acceleration=acceleration, throttle=throttle, brake=brake, time=time_current)
