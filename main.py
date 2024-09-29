d9726d931f4a6321a557045d692d481b9afe321d
NadulaG <51786507+NadulaG@users.noreply.github.com>
2024-09-29 13:33:26 -0400

    Should probably just use websockets for this tbh

--- main.py
+++ main.py
@@ -1,20 +1,71 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from datetime import datetime
+import time
+from multiprocessing import Process, Value
+
+speed = 0
+temperature = 0
+soc = 0
+odometer = 0
+acceleration = 0
+throttle = 0
+brake = 0
+time_current = datetime.now().strftime("%H:%M")
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def dashboard():
-    speed = 50
-    temperature = 25
-    soc = 80
-    odometer = 5
-    acceleration = 2
-    throttle = 77
-    brake = 19
-    time_current = datetime.now().strftime("%H:%M")
-    return render_dashboard(speed, temperature, soc, odometer, acceleration, throttle, brake, time_current)
+    return render_template('dashboard.html', speed=speed.value, temperature=temperature.value, soc=soc.value, odometer=odometer.value,
+                           acceleration=acceleration.value, throttle=throttle.value, brake=brake.value)
 
-def render_dashboard(speed, temperature, soc, odometer, acceleration, throttle, brake, time_current):
-    return render_template('dashboard.html', speed=speed, temperature=temperature, soc=soc, odometer=odometer,
-                           acceleration=acceleration, throttle=throttle, brake=brake, time=time_current)
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify({'speed': speed.value, 'temperature': temperature.value, 'soc': soc.value, 'odometer': odometer.value,
+                    'acceleration': acceleration.value, 'throttle': throttle.value, 'brake': brake.value})
+
+def update_status(speed, temperature, soc, odometer, acceleration, throttle, brake):
+    while True:
+        if (speed.value < 100):
+            speed.value += 1
+        else:
+            speed.value = 0
+
+        if (temperature.value < 100):
+            temperature.value += 1
+        else:
+            temperature.value = 0
+
+        if (soc.value < 100):
+            soc.value += 1
+        else:
+            soc.value = 0
+
+        if (odometer.value < 100):
+            odometer.value += 1
+        else:
+            odometer.value = 0
+
+        if (acceleration.value < 100):
+            acceleration.value += 1
+        else:
+            acceleration.value = 0
+
+        if (throttle.value < 100):
+            throttle.value += 1
+        else:
+            throttle.value = 0
+
+        if (brake.value < 100):
+            brake.value += 1
+        else:
+            brake.value = 0
+
+        time.sleep(0.1)
+
+if __name__ == '__main__':
+    speed, temperature, soc, odometer, acceleration, throttle, brake = Value('i', speed), Value('i', temperature), Value('i', soc), Value('i', odometer), Value('i', acceleration), Value('i', throttle), Value('i', brake)
+    p = Process(target=update_status, args=(speed, temperature, soc, odometer, acceleration, throttle, brake,))
+    p.start()
+    app.run(debug=True)
+    p.join()
--- static/script.js
+++ static/script.js
@@ -1,51 +1,33 @@
-// Function to generate random values for each property
-let speed = 0;
-let soc = 100;
+const speed = document.getElementById('speed');
+const temperature = document.getElementById('temperature');
+const soc = document.getElementById('soc');
+const odometer_acceleration = document.getElementById('odometer_acceleration');
+const throttle = document.getElementById('throttle');
+const brake = document.getElementById('brake');
+const time = document.getElementById('time');
+const throttle_bar = document.getElementById('throttle_bar');
+const brake_bar = document.getElementById('brake_bar');
 
-function updateValues() {
-  if (speed < 100) {
-    speed += 1;
-  } else {
-    speed = 0;
-  }
+async function updateValues() {
+  await fetch('/status', {
+    method: 'GET',
+  }).then((response) => {
+    response.json().then((data) => {
+      speed.innerHTML = data.speed;
+      temperature.innerHTML = `${data.temperature}°F`;
+      soc.innerHTML = data.soc;
+      odometer_acceleration.innerHTML = `${ data.odometer } miles · ${ data.acceleration } ft/s²`;
+      throttle.innerHTML = `${data.throttle}%`;
+      brake.innerHTML = `${data.brake}%`;
 
-  if (soc > 0) {
-    soc -= 1;
-  } else {
-    soc = 100;
-  }
+      // 100% throttle/braking should be height = 84px
+      throttle_bar.style.height = `${data.throttle * 0.84}px`;
+      brake_bar.style.height = `${data.brake * 0.84}px`;
+    });
+  });
 
-  const odometer = (Math.random() * 100).toFixed(1);
-  const acceleration = (Math.random() * 10).toFixed(1); // Random acceleration between 0-10 ft/s²
-  const throttle = Math.floor(Math.random() * 101); // Random throttle percentage between 0-100%
-  const brake = Math.floor(Math.random() * 101); // Random brake percentage between 0-100%
-  const temperature = Math.floor(Math.random() * 100); // Random temperature between 0-100°F
-  const time = new Date().toLocaleTimeString(); // Current time
-
-  document.querySelector("#speed").innerText = speed;
-
-  document.querySelector(
-    "#odometer_acceleration"
-  ).innerText = `${odometer} miles · ${acceleration} ft/s²`;
-
-  document.querySelector("#time").innerText = time.substring(
-    0,
-    time.length - 6
-  );
-
-  document.querySelector("#soc").innerText = `${soc}`;
-  document.querySelector("#soc").style.color =
-    soc <= 20 ? "#BE3838" : "#38be3f";
-
-  const battery_img = document.getElementById("#battery");
-  battery_img.src = soc <= 20 ? "/static/img/battery_red.svg" : "/static/img/battery_green.svg";
-
-  document.querySelector("#temperature").innerText = `${temperature}°F`;
-
-  document.querySelector("#throttle").innerText = `${throttle}%`;
-
-  document.querySelector("#brake").innerText = `${brake}%`;
+  const date = new Date();
+  time.innerHTML = `${date.getHours()}:${date.getMinutes() < 10 ? '0' : ''}${date.getMinutes()}`;
 }
 
-// Update values every second
-setInterval(updateValues, 150);
+setInterval(updateValues, 50);
